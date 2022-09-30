cef8233f1d7dbafebcb94a8a5dcef3abe2a5f55b
Hugo Josefson <hugo@josefson.org>
2022-09-30 21:28:38 +0200

    chore: i don't remember what i was doing

--- src/backends/wasm.rs
+++ src/backends/wasm.rs
@@ -1,52 +1,220 @@
 use cursive_core::backend;
-use cursive_core::event::Event;
+use cursive_core::event::{Event, Key, MouseButton, MouseEvent};
 use cursive_core::theme::{Color, ColorPair, Effect};
 use cursive_core::Vec2;
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
-pub struct Backend {}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(remote = "Key")]
+pub enum SerializableKey {
+    Enter,
+    Tab,
+    Backspace,
+    Esc,
+    Left,
+    Right,
+    Up,
+    Down,
+    Ins,
+    Del,
+    Home,
+    End,
+    PageUp,
+    PageDown,
+    PauseBreak,
+    NumpadCenter,
+    F0,
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12,
+}
 
+#[wasm_bindgen(typescript_custom_section)]
+const CURSIVE_BACKEND: &'static str = r#"
+interface CursiveBackend {
+    print(s: string): void;
+    setRaw(raw: boolean): void;
+}
+"#;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(typescript_type = "CursiveBackend")]
+    pub type CursiveBackend;
+
+    #[wasm_bindgen(structural, method, js_name = "pollEvent")]
+    pub fn poll_event(this: &CursiveBackend) -> String;
+
+    #[wasm_bindgen(structural, method, js_name = "setRaw")]
+    pub fn set_raw(this: &CursiveBackend, raw: bool);
+}
+
+pub struct Backend {
+    // js_backend: CursiveBackend,
+}
+
+#[wasm_bindgen]
+#[serde(with = "SerializableKey")]
 impl Backend {
-    pub fn init() -> Box<dyn backend::Backend> {
-        let c = Backend {};
-        Box::new(c)
-    }
-
-    fn parse_next(&mut self) -> Option<Event> {
-        None
+    // pub fn new(js_backend: CursiveBackend) -> Self {
+    //     Self { js_backend }
+    // }
+    fn hello(&mut self, s: &str) -> Key {
+        serde_json::from_str(s).unwrap()
     }
 }
-
-impl backend::Backend for Backend {
-    fn poll_event(&mut self) -> Option<Event> {
-        self.parse_next()
-    }
-
-    fn set_title(&mut self, _title: String) {}
-
-    fn refresh(&mut self) {}
-
-    fn has_colors(&self) -> bool {
-        true
-    }
-
-    fn screen_size(&self) -> Vec2 {
-        (80, 24).into()
-    }
-
-    fn print_at(&self, _pos: Vec2, _text: &str) {}
-
-    fn clear(&self, _color: Color) {}
-
-    fn set_color(&self, color: ColorPair) -> ColorPair {
-        color
-    }
-
-    fn set_effect(&self, _effect: Effect) {}
-
-    fn unset_effect(&self, _effect: Effect) {}
-
-    fn name(&self) -> &str {
-        "wasm"
-    }
-}
+//
+// impl backend::Backend for Backend {
+//     fn poll_event(&mut self) -> Option<Event> {
+//         let s = self.js_backend.poll_event();
+//         let event: SerializableEvent = serde_json::from_str(&s).unwrap();
+//         Some(event.into())
+//     }
+//
+//     fn set_title(&mut self, _title: String) {
+//         todo!()
+//     }
+//
+//     fn refresh(&mut self) {
+//         todo!()
+//     }
+//
+//     fn has_colors(&self) -> bool {
+//         todo!()
+//     }
+//
+//     fn screen_size(&self) -> Vec2 {
+//         todo!()
+//     }
+//
+//     fn print_at(&self, _pos: Vec2, _text: &str) {
+//         todo!()
+//     }
+//
+//     fn clear(&self, _color: Color) {
+//         todo!()
+//     }
+//
+//     fn set_color(&self, color: ColorPair) -> ColorPair {
+//         todo!()
+//     }
+//
+//     fn set_effect(&self, _effect: Effect) {
+//         todo!()
+//     }
+//
+//     fn unset_effect(&self, _effect: Effect) {
+//         todo!()
+//     }
+//
+//     fn name(&self) -> &str {
+//         "wasm"
+//     }
+// }
+//
+// #[derive(Serialize, Deserialize, Debug)]
+// #[serde(remote = "Event")]
+// pub enum SerializableEvent {
+//     WindowResize,
+//     FocusLost,
+//     Refresh,
+//     Char(char),
+//     CtrlChar(char),
+//     AltChar(char),
+//     #[serde(with = "SerializableKey")]
+//     Key(Key),
+//     #[serde(with = "SerializableKey")]
+//     Shift(Key),
+//     #[serde(with = "SerializableKey")]
+//     Alt(Key),
+//     #[serde(with = "SerializableKey")]
+//     AltShift(Key),
+//     #[serde(with = "SerializableKey")]
+//     Ctrl(Key),
+//     #[serde(with = "SerializableKey")]
+//     CtrlShift(Key),
+//     #[serde(with = "SerializableKey")]
+//     CtrlAlt(Key),
+//     Mouse {
+//         #[serde(with = "SerializableVec2")]
+//         offset: Vec2,
+//         #[serde(with = "SerializableVec2")]
+//         position: Vec2,
+//         #[serde(with = "SerializableMouseEvent")]
+//         event: MouseEvent,
+//     },
+//     Unknown(Vec<u8>),
+//     Exit,
+// }
+//
+// impl Into<Event> for SerializableEvent {
+//     fn into(self) -> Event {
+//         match self {
+//             SerializableEvent::WindowResize => Event::WindowResize,
+//             SerializableEvent::FocusLost => Event::FocusLost,
+//             SerializableEvent::Refresh => Event::Refresh,
+//             SerializableEvent::Char(c) => Event::Char(c),
+//             SerializableEvent::CtrlChar(c) => Event::CtrlChar(c),
+//             SerializableEvent::AltChar(c) => Event::AltChar(c),
+//             SerializableEvent::Key(k) => Event::Key(k),
+//             SerializableEvent::Shift(k) => Event::Shift(k),
+//             SerializableEvent::Alt(k) => Event::Alt(k),
+//             SerializableEvent::AltShift(k) => Event::AltShift(k),
+//             SerializableEvent::Ctrl(k) => Event::Ctrl(k),
+//             SerializableEvent::CtrlShift(k) => Event::CtrlShift(k),
+//             SerializableEvent::CtrlAlt(k) => Event::CtrlAlt(k),
+//             SerializableEvent::Mouse {
+//                 offset,
+//                 position,
+//                 event,
+//             } => Event::Mouse {
+//                 offset,
+//                 position,
+//                 event,
+//             },
+//             SerializableEvent::Unknown(bytes) => Event::Unknown(bytes),
+//             SerializableEvent::Exit => Event::Exit,
+//         }
+//     }
+// }
+//
+// #[derive(Serialize, Deserialize, Debug)]
+// #[serde(remote = "MouseEvent")]
+// enum SerializableMouseEvent {
+//     #[serde(with = "SerializableMouseButton")]
+//     Press(MouseButton),
+//     #[serde(with = "SerializableMouseButton")]
+//     Release(MouseButton),
+//     #[serde(with = "SerializableMouseButton")]
+//     Hold(MouseButton),
+//     WheelUp,
+//     WheelDown,
+// }
+//
+// #[derive(Serialize, Deserialize, Debug)]
+// #[serde(remote = "MouseButton")]
+// enum SerializableMouseButton {
+//     Left,
+//     Middle,
+//     Right,
+//     Button4,
+//     Button5,
+//     Other,
+// }
+//
+// #[derive(Serialize, Deserialize, Debug)]
+// #[serde(remote = "Vec2")]
+// pub struct SerializableVec2 {
+//     pub x: usize,
+//     pub y: usize,
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -12,71 +12,10 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 pub mod backends;
 
-mod cursive_ext;
-mod cursive_runnable;
-
-pub use cursive_ext::CursiveExt;
-pub use cursive_runnable::CursiveRunnable;
-
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn console_log(s: &str);
     #[wasm_bindgen(js_namespace = console, js_name = error)]
     fn console_error(s: &str);
-}
-
-#[wasm_bindgen]
-pub struct WasmCursive {
-    runnable: CursiveRunnable,
-}
-
-#[wasm_bindgen]
-impl WasmCursive {
-    pub fn new() -> Self {
-        let runnable = CursiveRunnable::wasm();
-        Self { runnable }
-    }
-    #[wasm_bindgen]
-    pub fn run(&mut self) {
-        self.runnable.run()
-    }
-}
-
-impl Default for WasmCursive {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-#[wasm_bindgen(js_name = "wasmCursive")]
-pub fn wasm_cursive() -> WasmCursive {
-    WasmCursive::new()
-}
-
-#[wasm_bindgen(typescript_custom_section)]
-const CURSIVE_BACKEND: &'static str = r#"
-interface CursiveBackend {
-    print(s: string): void;
-    setRaw(raw: boolean): void;
-}
-"#;
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(typescript_type = "CursiveBackend")]
-    pub type CursiveBackend;
-
-    #[wasm_bindgen(structural, method)]
-    pub fn print(this: &CursiveBackend, s: &str);
-
-    #[wasm_bindgen(structural, method, js_name = "setRaw")]
-    pub fn set_raw(this: &CursiveBackend, raw: bool);
-}
-
-#[wasm_bindgen(js_name = "doSomethingWithMyBackend")]
-pub fn do_something_with_my_backend(backend: &CursiveBackend) {
-    backend.set_raw(true);
-    backend.print("Hello from Rust!");
-    backend.set_raw(false);
 }
