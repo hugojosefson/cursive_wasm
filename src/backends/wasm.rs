1d49a6ebe0ec120661dbeedef8dfd324df5bff7a
Hugo Josefson <hugo@josefson.org>
2022-12-25 16:00:03 +0100

    docs: in code

--- src/backends/wasm.rs
+++ src/backends/wasm.rs
@@ -2,11 +2,19 @@ use wasm_bindgen::convert::{IntoWasmAbi, WasmAbi};
 use wasm_bindgen::describe::WasmDescribe;
 use wasm_bindgen::prelude::*;
 
+/**
+ * Call this function from JavaScript with a JavaScript implementation of `CursiveBackend`, to get
+ * back an instance of `Cursive` that uses your backend.
+ */
 #[wasm_bindgen(js_name = "wasmCursive")]
 pub fn wasm_cursive(js_backend: CursiveBackend) -> Backend {
     Backend::new(js_backend)
 }
 
+/**
+* This is what is printed into the `.d.ts` file.
+* Make sure it matches what is declared in the `extern "C"` block below!
+*/
 #[wasm_bindgen(typescript_custom_section)]
 const CURSIVE_BACKEND: &'static str = r#"
 interface CursiveBackend {
@@ -17,12 +25,21 @@ interface CursiveBackend {
 
 #[wasm_bindgen]
 extern "C" {
+    /**
+     * The Rust type `CursiveBackend`, for use by Rust code. Implementations of its methods are supplied from JavaScript.
+     */
     #[wasm_bindgen(typescript_type = "CursiveBackend")]
     pub type CursiveBackend;
 
+    /**
+     * Member of the `CursiveBackend` type.
+     */
     #[wasm_bindgen(structural, method, js_name = "setRaw")]
     pub fn set_raw(this: &CursiveBackend, raw: bool);
 
+    /**
+     * Member of the `CursiveBackend` type.
+     */
     #[wasm_bindgen(structural, method, js_name = "print")]
     pub fn print(this: &CursiveBackend, s: &str);
 }
@@ -31,6 +48,9 @@ pub struct Backend {
     js_backend: CursiveBackend,
 }
 
+/**
+ * This represents the `Cursive` type that JavaScript code will use.
+ */
 #[wasm_bindgen]
 impl Backend {
     pub fn new(js_backend: CursiveBackend) -> Self {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+// This module will be compiled into the main WASM module that backend implementors will import.
 #![feature(extern_types)]
 
 mod utils;
@@ -7,7 +8,9 @@ use wasm_bindgen::prelude::*;
 
 use crate::utils::set_panic_hook;
 
-// main function
+/**
+ * This function is exported from the main WASM module.
+ */
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     set_panic_hook();
@@ -22,6 +25,9 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 pub mod backends;
 
+/**
+ * This function is exported from the main WASM module.
+ */
 #[wasm_bindgen(js_name = "doSomethingWithMyBackend")]
 pub fn do_something_with_my_backend(backend: &CursiveBackend) {
     backend.set_raw(true);
